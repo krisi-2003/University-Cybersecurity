3f41ae4d3eccbe348656dd92d4cd938bf285adaa
Кристияна К <152493122+krisi-2003@users.noreply.github.com>
2025-03-15 13:12:58 +0200

    Първо упражнение
    Задача за намирране на прости числа

---
+++ prime.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<math.h>
+bool isPrime(int num)
+{
+    bool prime = true;
+    int end = sqrt(num);
+     for(int i =2;i<= end;i++)
+     {
+         if(num % i == 0)
+         {
+           prime = false;
+         }
+     }
+     return prime;
+    
+}
+
+int main()
+{
+   int num;
+   scanf("%d",&num);
+   bool res=isPrime(num);
+   if (res)
+   {
+    printf("The number is prime");
+   }
+   else
+   {
+    printf("The number is not prime");
+   }
+    return EXIT_SUCCESS;
+}
